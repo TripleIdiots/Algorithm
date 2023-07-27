15a304cda34f143e6d8ff32e4c7f01c053f03282
안정후 <austinan@naver.com>
2023-07-27 16:40:25 +0900

    feat: 백준 1267 B3

---
+++ AnJeongHoo/Java/src/main/java/com/algorithm/jeongh00/JAVAApplication.java
@@ -0,0 +1,13 @@
+package com.algorithm.jeongh00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JAVAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JAVAApplication.class, args);
+	}
+
+}
