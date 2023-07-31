0109aa58df566c00ed9349f202f1580ca6fab906
Yaklede <chh78151@naver.com>
2023-08-01 07:53:29 +0900

    feature: 11654 아스키코드 B5

---
+++ jimin/src/backjoon/BackJoon_11654.kt
@@ -0,0 +1,11 @@
+package src.backjoon
+
+import common.Solution
+
+class BackJoon_11654: Solution {
+    override fun solution() {
+        readln().toCharArray().forEach {
+            print(it.code)
+        }
+    }
+}
