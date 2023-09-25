991febee5d5979bce3f2f57b77681ad80b013dac
JeongYunSung <123dbstjd@gmail.com>
2023-09-25 22:26:46 +0900

    feature: back1010 kotlin S5

---
+++ JeongYunSung/Kotlin/src/main/kotlin/com/yunseong/algorithm/back1010/Back1010.kt
@@ -0,0 +1,47 @@
+package com.yunseong.algorithm.back1010
+
+import java.math.BigInteger
+import java.util.Scanner
+
+class Back1010 {
+
+    val map: HashMap<Int, BigInteger> = HashMap()
+
+    fun solve() {
+        val scanner = Scanner(System.`in`)
+
+        val round = scanner.nextLine().toInt()
+        var answer = ""
+
+        for (i in 0 until round) {
+            val values = scanner.nextLine()
+                .split(" ")
+                .map {
+                    it.toInt()
+                }
+
+            answer += (factorial(values[1]) / (factorial(values[0]) * factorial(values[1] - values[0]))).toString() + "\n"
+        }
+
+        println(answer)
+    }
+
+    fun factorial(n: Int): BigInteger {
+        if (n <= 1) {
+            return BigInteger.ONE
+        }
+
+        val value =
+            if (!map.containsKey(n)) {
+                val value = BigInteger.valueOf(n.toLong()).multiply(factorial(n - 1))
+                map[n] = value
+                value
+            } else map[n]!!
+
+        return value
+    }
+}
+
+fun main(args: Array<String>) {
+    Back1010().solve()
+}
