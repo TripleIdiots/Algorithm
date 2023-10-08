b8087bfb66251c8891782d57720816991a4a7806
Yaklede <chh78151@naver.com>
2023-10-08 23:09:41 +0900

    feature: N과M - 1

---
+++ jimin/src/backjoon/BackJoon_15649.kt
@@ -0,0 +1,38 @@
+package backjoon
+
+import common.Solution
+import java.lang.StringBuilder
+
+class BackJoon_15649: Solution {
+    var N = -1
+    var K = -1
+    val list = mutableListOf<Int>()
+    val result = StringBuilder()
+    fun create(temp: IntArray, idx: Int, visited: BooleanArray) {
+        if (idx == K) {
+            result.append(temp.joinToString(" ")).append("\n")
+            return
+        }
+
+        for (i: Int in 0 until N) {
+            if (!visited[i]) {
+                visited[i] = true
+                temp[idx] = list[i]
+                create(temp, idx + 1, visited)
+                visited[i] = false
+            }
+        }
+    }
+    override fun solution() {
+        val (A, B) = readln().split(" ").map { it.toInt() }
+        N = A
+        K = B
+        for (i: Int in 1..N) {
+            list.add(i)
+        }
+        val visited = BooleanArray(N) { false }
+        val numbers = IntArray(K) { 0 }
+        create(numbers, 0, visited)
+        println(result)
+    }
+}
