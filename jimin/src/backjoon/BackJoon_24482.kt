e20a58a0f00ae3327c3efc6ff1ef1fe5178d89ba
Yaklede <chh78151@naver.com>
2023-10-12 15:32:28 +0900

    feature: 깊이 우선 탐색 - 4

---
+++ jimin/src/backjoon/BackJoon_24482.kt
@@ -0,0 +1,49 @@
+package src.backjoon
+
+import common.Solution
+
+private lateinit var temp: IntArray
+class BackJoon_24482: Solution {
+    override fun solution() {
+        val (N, M, K) = readln().split(" ").map { it.toInt() }
+        val graph = Graph(N)
+        for (i: Int in 0 until M) {
+            val (x, y) = readln().split(" ").map { it.toInt() }
+            graph.putNode(x, y)
+        }
+        temp = IntArray(N + 1) { -1 }
+        graph.sorted()
+        graph.bfs(K, 0)
+        for (i: Int in 1..N) {
+            println(temp[i])
+        }
+    }
+
+    class Graph(n: Int) {
+        private val nodes = Array(n + 1) { ArrayList<Int>() }
+        private val visited = BooleanArray(n + 1)
+
+        fun putNode(x: Int, y: Int) {
+            this.nodes[x].add(y)
+            this.nodes[y].add(x)
+        }
+
+        fun bfs(index: Int, depth: Int) {
+            temp[index] = depth
+            visited[index] = true
+            val ints = this.nodes[index]
+            for (int in ints) {
+                if (!visited[int]) {
+                    visited[int] = true
+                    bfs(int, depth + 1)
+                }
+            }
+        }
+
+        fun sorted() {
+            for (element in nodes) {
+                element.sortDescending()
+            }
+        }
+    }
+}
