cc84cfc14bc9963fc4315cc6659fc9c1a7714ef9
안정후 <austinan@naver.com>
2024-03-15 15:32:11 +0900

    feat: update debitcard

--- AnJeongHoo/Java/src/main/java/com/algorithm/jeongh00/python/DebitCard.py
+++ AnJeongHoo/Java/src/main/java/com/algorithm/jeongh00/python/DebitCard.py
@@ -1,8 +1,42 @@
+class CreditCard:
+
+    def __init__(self, customer, bank, account, limit, balance):
+        self._customer = customer
+        self._bank = bank
+        self._account = account
+        self._limit = limit
+        self._balance = balance
+
+    def get_customer(self):
+        return self._customer
+
+    def get_bank(self):
+        return self._bank
+
+    def get_account(self):
+        return self._account
+
+    def get_limit(self):
+        return self._limit
+
+    def get_balance(self):
+        return self._balance
+
+    def charge(self, price):
+        if price + self._balance > self._limit:
+            return False
+        else:
+            self._balance += price
+            return True
+
+    def make_payment(self, amount):
+        self._balance -= amount
+
 class DebitCard(CreditCard):
 
-    def __init__(self, customer, bank, account, limit, monthly_interest_rate, monthly_interest_payment_date):
+    def __init__(self, customer, bank, account, limit, balance, monthly_interest_rate, monthly_interest_payment_date):
 
-        super().__init__(customer, bank, account, limit, balance=0)
+        super().__init__(customer, bank, account, limit, balance)
         self._monthly_interest_rate = monthly_interest_rate  # 월 이자율
         self._monthly_interest_payment_date = monthly_interest_payment_date  # 월 이자 지급 일자
 
@@ -30,3 +64,26 @@ class DebitCard(CreditCard):
         interest = self._calculate_monthly_interest()
         self._balance += interest
         print(f"이번 달 이자 {interest} 는 {self._monthly_interest_payment_date} 에 지급됩니다!!")
+
+def main():
+    # DebitCard 객체 생성
+    debit_card = DebitCard("Jeonghoo An", "Best Bank", "1234567890", 1000, 500, 0.05, 15)
+
+    # 현재 잔액 확인
+    print("현재 잔액:", debit_card.get_balance())
+
+    # 입금 시도
+    debit_card.deposit(200)
+    print("입금 후 잔액:", debit_card.get_balance())
+
+    # 출금 시도
+    debit_card.withdraw(100)
+    print("출금 후 잔액:", debit_card.get_balance())
+
+    # 월 이자 계산 및 지급
+    interest = debit_card.calculate_monthly_interest()
+    print("이번 달 이자:", interest)
+    debit_card.pay_monthly_interest()
+    print("이자 지급 후 잔액:", debit_card.get_balance())
+
+main()
